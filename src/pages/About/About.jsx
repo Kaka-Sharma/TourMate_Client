3d9829b507cec86b3e95354ec5be31555a6286f6
Kaka-Sharma <pallavsharma407@gmail.com>
2026-05-16 18:04:49 +0530

    updated import

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./About.module.css";
-import featureImg from "../../../public/BackGround2.png";
+import featureImg from "/BackGround2.png";
 import {
   FaMapMarkedAlt,
   FaShieldAlt,
